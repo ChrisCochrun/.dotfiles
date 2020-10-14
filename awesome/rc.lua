572ec2a8bfe0a12026821cbab721cac927119a9c
Chris Cochrun <chris@tfcconnection.org>
2020-10-14 06:51:56 -0500

    making sure fennel loads

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,6 +1,7 @@
+pcall(require, "luarocks.loader")
 fennel = require("fennel")
 local gears = require("gears")
 fennel.path = fennel.path .. ";.config/awesome/?.fnl"
-table.insert(package.loaders or package.searchers, fennel.searcher)
+table.insert(package.loaders or package.searchers, fennel.make_searcher({correlate=true}))
 
 require("init") -- load ~/.config/awesome/config.fnl
